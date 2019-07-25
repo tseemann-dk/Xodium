3161a4ecfa9f0bb69498310194e29175a9c2ef29
Torsten Seemann <tseemann@live.dk>
2019-07-26 00:03:20 +0200

    Organized Xodium solution into subfolders, updated Xodium package icons and added initial Sidekick sample

---
+++ source/Sidekick/Sidekick/AppShell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Sidekick
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ source/Sidekick/Sidekick/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Sidekick.Models;
+
+namespace Sidekick.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

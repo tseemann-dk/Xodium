8fe33e595c40b9bd7957e5459c110ed78e6d7e46
Torsten Seemann <tseemann@live.dk>
2019-09-20 23:37:22 +0200

    Added preliminary Uno UI

---
+++ source/Sidekick/Sidekick.Shopper.UI.Uno/Views/ShoppingGroupView.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+
+namespace Sidekick.Shopper.UI.Uno.Views
+{
+    public sealed partial class ShoppingGroupView : UserControl
+    {
+        public ShoppingGroupView()
+        {
+            this.InitializeComponent();
+
+            DataContext = new
+            {
+                Nodes = Enumerable.Range(1, 100).Select(x => new
+                {
+                    Number = x.ToString(),
+                    Text = $"Node {x}"
+                })
+            };
+        }
+    }
+}

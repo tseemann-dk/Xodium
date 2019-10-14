8d7695980197eaba7369e824d646ccf10fce8097
Torsten Seemann <TSE@seas-nve.dk>
2019-10-14 10:18:35 +0200

    Updated tests

--- source/Xodium.Core.Tests/Models/SampleNode.cs
+++ source/Xodium.Core.Tests/Models/SampleNode.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Xodium.Core.Tests.Utilities;
 using Xodium.DataStructures;
 
 namespace Xodium.Core.Tests.Models
--- source/Xodium.Core.Tests/TreeInspectionTest.cs
+++ source/Xodium.Core.Tests/TreeInspectionTest.cs
@@ -3,7 +3,6 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Xodium.Core.Tests.Models;
-using Xodium.Core.Tests.Utilities;
 using Xodium.DataStructures;
 using Xunit;
 

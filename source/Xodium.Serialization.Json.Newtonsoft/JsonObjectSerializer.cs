99390e136468b8a27208bb8a66c9dd41a559c84e
Torsten Seemann <TSE@seas-nve.dk>
2020-03-01 23:03:51 +0100

    Added JsonObjectSerializer constructor

--- source/Xodium.Serialization.Json.Newtonsoft/JsonObjectSerializer.cs
+++ source/Xodium.Serialization.Json.Newtonsoft/JsonObjectSerializer.cs
@@ -6,32 +6,31 @@ namespace Xodium.Serialization.Json.Newtonsoft
 {
     public class JsonObjectSerializer : IObjectSerializer
     {
+        private readonly JsonSerializer serializer;
+
+        public JsonObjectSerializer(JsonSerializer serializer = null)
+        {
+            this.serializer = serializer ?? JsonSerializer.CreateDefault();
+        }
+
         public async Task Serialize<T>(T obj, Stream stream)
         {
             using (var streamWriter = new StreamWriter(stream))
+            using (var jsonWriter = new JsonTextWriter(streamWriter))
             {
-                var serializer = JsonSerializer.CreateDefault();
-
-                using (var jsonWriter = new JsonTextWriter(streamWriter))
-                {
-                    serializer.Serialize(jsonWriter, obj);
-                    await streamWriter.FlushAsync().ConfigureAwait(false);
-                    stream.Position = 0;
-                }
+                serializer.Serialize(jsonWriter, obj);
+                await streamWriter.FlushAsync().ConfigureAwait(false);
+                stream.Position = 0;
             }
         }
 
         public Task<T> Deserialize<T>(Stream stream)
         {
             using (var streamReader = new StreamReader(stream))
+            using (var jsonReader = new JsonTextReader(streamReader))
             {
-                using (var jsonReader = new JsonTextReader(streamReader))
-                {
-                    var serializer = JsonSerializer.CreateDefault();
-                    var obj = serializer.Deserialize<T>(jsonReader);
-
-                    return Task.FromResult(obj);
-                }
+                var obj = serializer.Deserialize<T>(jsonReader);
+                return Task.FromResult(obj);
             }
         }
     }

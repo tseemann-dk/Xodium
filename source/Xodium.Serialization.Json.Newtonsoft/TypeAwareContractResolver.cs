f85983ac4c3d7b1a2a1d32ca40e6c763ea5cddfe
Torsten Seemann <TSE@seas-nve.dk>
2021-04-03 17:56:24 +0200

    Added Newtonsoft TypeAwareContractResolver

---
+++ source/Xodium.Serialization.Json.Newtonsoft/TypeAwareContractResolver.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Xodium.Serialization.Json.Newtonsoft
+{
+    public class TypeAwareContractResolver : DefaultContractResolver
+    {
+        private readonly string typeDiscriminator;
+
+        public TypeAwareContractResolver(string typeDiscriminator)
+        {
+            this.typeDiscriminator = typeDiscriminator ?? throw new ArgumentNullException(nameof(typeDiscriminator));
+            NamingStrategy = new CamelCaseNamingStrategy();
+        }
+
+        protected override IList<JsonProperty> CreateProperties(Type type, MemberSerialization memberSerialization)
+        {
+            return base.CreateProperties(type, memberSerialization)
+                .Where(ShouldIncludeProperty)
+                .OrderBy(x => !IsTypeDiscriminator(x))
+                .ToList();
+        }
+
+        protected bool ShouldIncludeProperty(JsonProperty property)
+        {
+            return IsTypeDiscriminator(property) || property.Writable;
+        }
+
+        protected bool IsTypeDiscriminator(JsonProperty property)
+        {
+            return string.Compare(property.PropertyName, typeDiscriminator, true, CultureInfo.InvariantCulture) == 0;
+        }
+    }
+}

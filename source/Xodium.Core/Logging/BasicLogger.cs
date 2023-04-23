ec4b2a59dd4d1764105a615ceb1ea288fdb83c2b
Torsten Seemann <tseemann@live.dk>
2023-04-23 10:37:10 +0200

    Added Logging (WIP)

---
+++ source/Xodium.Core/Logging/BasicLogger.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Xodium.Logging
+{
+    public class BasicLogger : ILogger
+    {
+        private readonly TextWriter output;
+
+        public BasicLogger(TextWriter output)
+        {
+            this.output = output ?? throw new System.ArgumentNullException(nameof(output));
+        }
+
+        public void Log(string message) => output.WriteLine(message);
+        public Task LogAsync(string message) => output.WriteLineAsync(message);
+    }
+}
---
+++ source/Xodium.Core/Logging/BasicLoggingContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Xodium.Logging
+{
+    public class BasicLoggingContext : ILoggingContext
+    {
+        private readonly TextWriter output;
+
+        public BasicLoggingContext(TextWriter output)
+        {
+            this.output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        public async Task<ILoggingScope> CreateScope() => await Task.FromResult(new BasicLoggingScope(output));
+    }
+}

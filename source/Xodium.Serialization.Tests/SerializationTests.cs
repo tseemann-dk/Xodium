7dc572316becee3a3f679dd156c4331cfa5588fd
Torsten Seemann <tseemann@live.dk>
2023-07-15 13:23:33 +0200

    Added ObjectToInferredTypesConverter

---
+++ source/Xodium.Serialization.Tests/SerializationTests.cs
@@ -0,0 +1,83 @@
+using FluentAssertions;
+using Nsj = Newtonsoft.Json;
+using Stj = System.Text.Json;
+
+namespace Xodium.Serialization.Tests
+{
+    public class SerializationTests
+    {
+        [Theory]
+        [MemberData(nameof(Serializers))]
+        public async Task CanSerializeAndDeserializePlainObject(IObjectSerializer serializer)
+        {
+            var obj1 = CreatePerson();
+            var str = await serializer.SerializeAsync(obj1);
+            var obj2 = await serializer.DeserializeAsync<Person>(str);
+
+            obj2.Should().BeEquivalentTo(obj1);
+        }
+
+        [Theory]
+        [MemberData(nameof(Serializers))]
+        public async Task CanSerializeAndDeserializeUntypedData(IObjectSerializer serializer)
+        {
+            var obj1 = CreateUntypedData();
+            var str = await serializer.SerializeAsync(obj1);
+            var obj2 = await serializer.DeserializeAsync<UntypedData>(str);
+
+            obj2.Should().BeEquivalentTo(obj1);
+        }
+
+        public static IEnumerable<object[]> Serializers => new[]
+        {
+            new object[] { CreateCoreFXJsonSerializer() },
+            new object[] { CreateNewtonsoftJsonSerializer() }
+        };
+
+        private static Json.Newtonsoft.JsonObjectSerializer CreateNewtonsoftJsonSerializer()
+        {
+            var settings = new Nsj.JsonSerializerSettings
+            {
+            };
+
+            return new Json.Newtonsoft.JsonObjectSerializer();
+        }
+
+        private static Json.CoreFX.JsonObjectSerializer CreateCoreFXJsonSerializer()
+        {
+            var options = new Stj.JsonSerializerOptions
+            {
+                Converters =
+                {
+                    new Json.CoreFX.ObjectToInferredTypesConverter()
+                }
+            };
+
+            return new Json.CoreFX.JsonObjectSerializer(options);
+        }
+
+        private static Person CreatePerson() => new()
+        {
+            Id = 1,
+            Name = "John Doe"
+        };
+
+        private static UntypedData CreateUntypedData() => new()
+        {
+            Value1 = 123,
+            Value2 = "Sample Text"
+        };
+
+        private class Person
+        {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+        }
+
+        private class UntypedData
+        {
+            public object? Value1 { get; set; }
+            public object? Value2 { get; set; }
+        }
+    }
+}
